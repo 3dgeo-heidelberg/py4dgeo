bad07bc9e2470e53d17c0b198c0d01c19ee01d20
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-07-13 16:30:48 +0200

    Write a C++ and Python test for the KDTree

--- tests/c++/py4dgeo_t.cpp
+++ tests/c++/py4dgeo_t.cpp
@@ -1,9 +1,41 @@
 #include "catch2/catch.hpp"
 #include "py4dgeo/py4dgeo.hpp"
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace py4dgeo;
 
-TEST_CASE("dummy", "[dummy]")
+TEST_CASE("KDTree is correctly build", "[kdtree]")
 {
-  REQUIRE(true);
+  // Load a test case
+  std::vector<double> data;
+  std::ifstream stream("../data/plane_horizontal_t1.xyz");
+  std::string line;
+  std::size_t points{ 0 };
+
+  while (std::getline(stream, line)) {
+    std::istringstream s(line);
+    double x;
+    for (int i = 0; i < 3; ++i) {
+      s >> x;
+      data.push_back(x);
+    }
+    ++points;
+  }
+
+  // Construct the KDTree
+  KDTree tree(data.data(), points);
+  tree.build_tree(10);
+
+  // Find all nodes with a radius search
+  std::array<double, 3> o{ 0.0, 0.0, 0.0 };
+  std::vector<std::pair<std::size_t, double>> result;
+
+  auto num = tree.radius_search(o.data(), 100.0, result);
+
+  REQUIRE(num == points);
+  REQUIRE(result.size() == points);
 }
--- tests/python/test_py4dgeo.py
+++ tests/python/test_py4dgeo.py
@@ -1,5 +1,19 @@
 import py4dgeo
 
+import numpy as np
+import os
+import pytest
 
-def test_dummy():
-    assert True
+
+_test_files = [
+    os.path.join(os.path.split(__file__)[0], "../data/plane_horizontal_t1.xyz")
+]
+
+
+@pytest.mark.parametrize("filename", _test_files)
+def test_kdtree(filename):
+    data = np.genfromtxt(filename)
+    tree = py4dgeo.KDTree(data)
+    tree.build_tree(10)
+    result = tree.radius_search(np.array([0, 0, 0]), 100)
+    assert result[0].shape[0] == data.shape[0]
