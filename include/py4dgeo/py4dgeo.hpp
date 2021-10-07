4c2b5ca4a3e621e7a98011261764579952eadb28
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-10-07 09:11:24 +0200

    Document types in py4dgeo.hpp

--- include/py4dgeo/py4dgeo.hpp
+++ include/py4dgeo/py4dgeo.hpp
@@ -4,13 +4,54 @@
 
 namespace py4dgeo {
 
-// The types we use for Point Clouds on the C++ side
+/* Declare the most important types used in py4dgeo */
+
+/** @brief The C++ type for a point cloud
+ *
+ * Point clouds are represented as (nx3) matrices from the Eigen library.
+ *
+ * The choice of this type allows us both very efficient implementation
+ * of numeric algorithms using the Eigen library, as well as no-copy
+ * interoperability with numpy's multidimensional arrays.
+ */
 using EigenPointCloud =
   Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>;
+
+/** @brief A non-const reference type for passing around @ref EigenPointCloud
+ *
+ * You should use this in function signatures that accept a point cloud
+ * as a parameter and need to modify the point cloud.
+ */
 using EigenPointCloudRef = Eigen::Ref<EigenPointCloud>;
+
+/** @brief A const reference type for passing around @ref EigenPointCloud
+ *
+ * You should use this in function signatures that accept a read-only
+ * point cloud.
+ */
 using EigenPointCloudConstRef = const Eigen::Ref<const EigenPointCloud>&;
+
+/** @brief A vector of dynamic size
+ *
+ * The choice of this type allows us both very efficient implementation
+ * of numeric algorithms using the Eigen library, as well as no-copy
+ * interoperability with numpy's arrays.
+ */
 using EigenVector = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+
+/** @brief A non-const reference type for passing around @ref EigenVector
+ *
+ * You should use this in function signatures that accept and modify a vector.
+ */
 using EigenVectorRef = Eigen::Ref<EigenVector>;
+
+/** @brief A const reference type for passing around @ref EigenVector
+ *
+ * You should use this in function signatures that accept a read-only vector.
+ */
+using EigenVectorConstRef = const Eigen::Ref<const EigenVector>&;
+
+/** @brief The type used for point cloud indices */
 using IndexType = Eigen::Index;
 
 }
