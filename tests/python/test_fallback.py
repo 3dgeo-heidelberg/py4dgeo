f41716b62cda552f6e3babd7f938de262d147966
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-10-07 10:16:40 +0200

    Add a test for the Python fallback

---
+++ tests/python/test_fallback.py
@@ -0,0 +1,19 @@
+from py4dgeo.fallback import *
+from py4dgeo.m3c2 import M3C2
+
+from . import epoch1, epoch2
+
+
+def test_python_fallback_m3c2(epoch1, epoch2):
+    # Instantiate a fallback M3C2 instance
+    pym3c2 = PythonFallbackM3C2(
+        epochs=(epoch1, epoch2), corepoints=epoch1.cloud, radii=(3.0,), scales=(2.0,)
+    )
+
+    # And a regular C++ based one
+    m3c2 = M3C2(
+        epochs=(epoch1, epoch2), corepoints=epoch1.cloud, radii=(3.0,), scales=(2.0,)
+    )
+
+    # The results should match
+    assert (m3c2.run() == pym3c2.run()).all()
