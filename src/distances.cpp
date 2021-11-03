d133c63ff4152b68076a54ce5159bb303869b5a6
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-11-03 14:34:47 +0100

    Make distances a signed quantity

--- src/distances.cpp
+++ src/distances.cpp
@@ -33,8 +33,7 @@ compute_distances(EigenPointCloudConstRef corepoints,
       epoch2, scale, corepoints.row(i), dir, max_cylinder_length, i);
 
     // Distance calculation
-    double dist = dir.dot(subset1.colwise().mean() - subset2.colwise().mean());
-    distances[i] = std::abs(dist);
+    distances[i] = dir.dot(subset1.colwise().mean() - subset2.colwise().mean());
 
     // Uncertainty calculation
     uncertainties[i] = uncertaintycalculator(subset1, subset2, dir);
