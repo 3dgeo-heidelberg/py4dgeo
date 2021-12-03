9640b097c891fd3ed9225d90d852d149fa043e48
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-12-03 14:02:29 +0100

    Remove redundant test setup implementation

--- tests/c++/testsetup.cpp
+++ tests/c++/testsetup.cpp
@@ -50,23 +50,6 @@ ahk_benchcloud()
 std::pair<std::shared_ptr<EigenPointCloud>, std::shared_ptr<EigenPointCloud>>
 testcloud()
 {
-  std::vector<float> data;
-  std::ifstream stream(DATAPATH(plane_horizontal_t1.xyz));
-  std::string line;
-  std::size_t points{ 0 };
-
-  while (std::getline(stream, line)) {
-    std::istringstream s(line);
-    float x;
-    for (int i = 0; i < 3; ++i) {
-      s >> x;
-      data.push_back(x);
-    }
-    ++points;
-  }
-
-  // Interpret the given data as an Eigen matrix
-  auto cloud = std::make_shared<EigenPointCloud>(points, 3);
-  std::copy(data.data(), data.data() + points * 3, cloud->data());
+  auto cloud = benchcloud_from_file(DATAPATH(plane_horizontal_t1.xyz));
   return std::make_pair(cloud, cloud);
 }
