eac74ac743f8de2c30c154acf09820764c18c5ad
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-12-10 15:05:01 +0100

    Use omp critical instead of std::mutex
    Mixing C++11 threading with OpenMP is undefined behaviour.

--- include/py4dgeo/openmp.hpp
+++ include/py4dgeo/openmp.hpp
@@ -5,24 +5,21 @@
 #endif
 
 #include <exception>
-#include <mutex>
 
 /** @brief A container to handle exceptions in parallel regions
  *
  *  OpenMP does have limited support for C++ exceptions in parallel
  *  regions: Exceptions need to be catched on the same thread they
  *  have been thrown on. This class allows to store the first thrown
- *  exception in a thread-safe manner to then rethrow it after we
- *  left the parallel region. This is a necessary construct to
- *  propagate exceptions from Python callbacks through the multithreaded
- *  C++ layer back to the calling Python scope. Inspiration is taken
- *  from:
- * https://stackoverflow.com/questions/11828539/elegant-exceptionhandling-in-openmp
+ *  exception to then rethrow it after we left the parallel region.
+ *  This is a necessary construct to propagate exceptions from Python
+ *  callbacks through the multithreaded C++ layer back to the calling
+ *  Python scope. Inspiration is taken from:
+ *  https://stackoverflow.com/questions/11828539/elegant-exceptionhandling-in-openmp
  */
 class CallbackExceptionVault
 {
   std::exception_ptr ptr = nullptr;
-  std::mutex lock;
 
 public:
   template<typename Function, typename... Parameters>
@@ -31,9 +28,13 @@ public:
     try {
       std::forward<Function>(f)(std::forward<Parameters>(parameters)...);
     } catch (...) {
-      std::unique_lock<std::mutex> guard(this->lock);
-      if (!this->ptr)
-        this->ptr = std::current_exception();
+#ifdef PY4DGEO_WITH_OPENMP
+#pragma omp critical
+#endif
+      {
+        if (!this->ptr)
+          this->ptr = std::current_exception();
+      }
     }
   }
 
