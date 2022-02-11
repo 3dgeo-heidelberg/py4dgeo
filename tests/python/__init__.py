12b4a88b645b5dcddfe86af16dce0409c79c3f86
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-02-11 13:26:31 +0100

    Add an M3C2 algorithm class that uses alternative CC-inspired names

--- tests/python/__init__.py
+++ tests/python/__init__.py
@@ -1,5 +1,6 @@
 from py4dgeo.epoch import read_from_xyz
 
+import numpy as np
 import os
 import pytest
 
@@ -24,3 +25,17 @@ def epoch_fixture(*filenames):
 
 # Instantiate one fixture per data dile
 epochs = epoch_fixture("plane_horizontal_t1.xyz", "plane_horizontal_t2.xyz")
+
+
+def compare_algorithms(alg1, alg2):
+    """A helper to compare the output of two algorithms that should be equivalent"""
+    # Run the two algorithms
+    distances1, uncertainties1 = alg1.run()
+    distances2, uncertainties2 = alg2.run()
+
+    assert np.allclose(distances1, distances2)
+    assert np.allclose(uncertainties1["lodetection"], uncertainties2["lodetection"])
+    assert np.allclose(uncertainties1["stddev1"], uncertainties2["stddev1"])
+    assert np.allclose(uncertainties1["stddev2"], uncertainties2["stddev2"])
+    assert np.allclose(uncertainties1["num_samples1"], uncertainties2["num_samples1"])
+    assert np.allclose(uncertainties1["num_samples2"], uncertainties2["num_samples2"])
---
+++ tests/python/test_cloudcompare.py
@@ -0,0 +1,26 @@
+from py4dgeo.cloudcompare import CloudCompareM3C2
+from py4dgeo.m3c2 import M3C2
+
+from . import compare_algorithms, epochs
+
+
+def test_cloudcompare_m3c2(epochs):
+    epoch1, epoch2 = epochs
+
+    # Instantiate an M3C2 instance
+    m3c2 = M3C2(
+        epochs=(epoch1, epoch2),
+        corepoints=epoch1.cloud,
+        cyl_radii=(3.0,),
+        normal_radii=(2.0,),
+    )
+
+    # Instantiate Cloud compare variant
+    cc_m3c2 = CloudCompareM3C2(
+        epochs=(epoch1, epoch2),
+        corepoints=epoch1.cloud,
+        searchscale=(3.0,),
+        normalscale=(2.0,),
+    )
+
+    compare_algorithms(m3c2, cc_m3c2)
