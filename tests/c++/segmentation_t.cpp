a585f08141c7f793cc9256552946ddf7f019b80d
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-06-01 16:21:36 +0200

    Adapt C++ tests

--- tests/c++/segmentation_t.cpp
+++ tests/c++/segmentation_t.cpp
@@ -12,7 +12,7 @@ TEST_CASE("DTW distance calculation", "[segmentation]")
   EigenSpatiotemporalArray arr(2, 4);
   arr << 1, 5, 4, 2, 1, 2, 4, 1;
 
-  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1) };
+  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1), 0.0, 0.0 };
   auto dist = dtw_distance(data);
 
   REQUIRE(dist > 0);
@@ -24,7 +24,7 @@ TEST_CASE("DTW distance with NaN Values", "[segmentation]")
   const auto nan = std::numeric_limits<double>::quiet_NaN();
   EigenSpatiotemporalArray arr(2, 6);
   arr << nan, 1, 42, 5, 4, 2, 42, 1, nan, 2, 4, 1;
-  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1) };
+  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1), 0.0, 0.0 };
   auto dist = dtw_distance(data);
   REQUIRE(std::abs(dist - 3) < 1e-8);
 }
@@ -34,7 +34,7 @@ TEST_CASE("DTW distance with all NaN Values", "[segmentation]")
   const auto nan = std::numeric_limits<double>::quiet_NaN();
   EigenSpatiotemporalArray arr(2, 2);
   arr << nan, nan, nan, nan;
-  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1) };
+  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1), 0.0, 0.0 };
   auto dist = dtw_distance(data);
   REQUIRE(std::isnan(dist));
 }
@@ -44,7 +44,7 @@ TEST_CASE("Normalized DTW Distances", "[segmentation]")
   const auto nan = std::numeric_limits<double>::quiet_NaN();
   EigenSpatiotemporalArray arr(2, 6);
   arr << nan, 1, 42, 5, 4, 2, 42, 1, nan, 2, 4, 1;
-  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1) };
+  TimeseriesDistanceFunctionData data{ arr.row(0), arr.row(1), 0.0, 0.0 };
   auto dist = normalized_dtw_distance(data);
   REQUIRE(dist >= 0.0);
   REQUIRE(dist <= 1.0);
