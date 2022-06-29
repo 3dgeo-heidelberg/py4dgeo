45b45a23ebe4cdcea97f9fe96114c8cdd0c12600
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-06-29 17:03:27 +0200

    Bump version to v0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 
 setup(
     name="py4dgeo",
-    version="0.3.0",
+    version="0.4.0",
     author="Dominic Kempf",
     author_email="ssc@iwr.uni-heidelberg.de",
     description="Library for change detection in 4D point cloud data",
