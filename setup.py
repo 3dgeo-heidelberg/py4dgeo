ac7cd394358b2dfb7d3a6c14707458a1c76e367f
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-07-05 11:37:48 +0200

    Partial return to specifying configuration in setup.py
    As scikit-build does not yet support PEP621.

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from skbuild import setup
 
 
 setup(
+    packages=["py4dgeo"],
+    package_dir={"": "src"},
+    zip_safe=False,
     cmake_args=[
         "-DBUILD_DOCS=OFF",
         "-DBUILD_TESTING=OFF",
