b789fd85d77d3f6727e95679bbacb2b4e4a607cc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 01:13:31 +0000

    Adding tarfile member sanitization to extractall()

--- src/py4dgeo/util.py
+++ src/py4dgeo/util.py
@@ -255,7 +255,26 @@ def copy_test_data(target):
             tar.write(archive)
 
         with tarfile.open(archive_file, "r:gz") as tar:
-            tar.extractall(path=target)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=target)
 
 
 def copy_test_data_entrypoint():
