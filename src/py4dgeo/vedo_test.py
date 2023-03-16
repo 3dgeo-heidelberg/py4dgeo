d3a17edf7e63d879587c1c401697c23587c64deb
Horea Caramizaru <horea.caramizaru@cti.pub.ro>
2023-03-16 14:29:31 +0100

    update

--- src/py4dgeo/vedo_test.py
+++ src/py4dgeo/vedo_test.py
@@ -5,13 +5,14 @@ from vedo import *
 
 PreviousSelection = None
 
-def func(evt): # called every time the mouse moves
+
+def func(evt):  # called every time the mouse moves
     # evt is a dotted dictionary
 
-    if evt.keyPressed == 'z':
+    if evt.keyPressed == "z":
         print("reset")
-        button.status( button.states )
-        #button.switch()
+        button.status(button.states)
+        # button.switch()
         pass
 
     if not evt.actor:
@@ -21,27 +22,28 @@ def func(evt): # called every time the mouse moves
         print(evt.actor)
     # print("full event dump:", evt)
 
+
 util.ensure_test_data_availability()
 Epoch0, Epoch1 = read_from_xyz("plane_horizontal_t1.xyz", "plane_horizontal_t2.xyz")
 
 
 plt = Plotter(axes=3)
-plt.add_callback('EndInteraction', func)
-plt.add_callback('KeyRelease', func)
+plt.add_callback("EndInteraction", func)
+plt.add_callback("KeyRelease", func)
 
 button = plt.add_button(
-    lambda :None,
+    lambda: None,
     pos=(0.9, 0.9),  # x,y fraction from bottom left corner
     states=["Reset", "Epoch A", "Epoch B"],
     c=["g", "r"],
     bc=["dg", "dv"],  # colors of states
-    font="courier",   # arial, courier, times
+    font="courier",  # arial, courier, times
     size=25,
     bold=True,
     italic=False,
 )
 
-#plt.show([Points(Epoch0.cloud), Points(Epoch1.cloud)]).close()
+# plt.show([Points(Epoch0.cloud), Points(Epoch1.cloud)]).close()
 plt.show([Points(Epoch0.cloud, c=(1, 0, 1)), Points(Epoch1.cloud, c=(0, 1, 0))]).close()
 
-#plt.show(elli).close()
+# plt.show(elli).close()
