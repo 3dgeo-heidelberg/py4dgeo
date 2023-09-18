7b7e14341898da62dc6a6dc344123e083cffd815
dkapitan <madgfess@gmail.com>
2023-09-18 13:33:57 +0200

    Added functions for calculation ICP method

--- src/py4dgeo/__init__.py
+++ src/py4dgeo/__init__.py
@@ -9,7 +9,12 @@ from py4dgeo.epoch import (
 )
 from py4dgeo.m3c2 import M3C2, write_m3c2_results_to_las
 from py4dgeo.m3c2ep import M3C2EP
-from py4dgeo.registration import iterative_closest_point
+from py4dgeo.registration import (
+    iterative_closest_point,
+    point_to_plane_icp,
+    point_to_plane_icp_LM,
+    p_to_p_icp,
+)
 from py4dgeo.segmentation import (
     RegionGrowingAlgorithm,
     SpatiotemporalAnalysis,
