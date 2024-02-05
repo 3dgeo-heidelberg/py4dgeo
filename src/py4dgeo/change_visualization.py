bf23fdb46a1fb61caf0a053c77b2952fb9d111ed
Ronald Tabernig <ronald.tabernig@gmail.com>
2024-02-05 16:33:48 +0100

    VZ update visualization module, add new plot type

--- src/py4dgeo/change_visualization.py
+++ src/py4dgeo/change_visualization.py
@@ -384,7 +384,7 @@ class ChangeEventValidator:
                 validated_data = validate_model(ChangeEvent, element)
 
                 # Create ChangeEvent object
-                change_event = ChangeEvent(**validated_data[0])
+                change_event = ChangeEvent(**validated_data)
                 change_events.append(change_event)
 
             return change_events
