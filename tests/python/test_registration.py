86c2e63eac0fc148c17423cf12b97a77a007a4a5
DimasfromLavoisier <madgfess@gmail.com>
2024-04-08 14:18:51 +0200

    upd test

--- tests/python/test_registration.py
+++ tests/python/test_registration.py
@@ -3,7 +3,9 @@ from py4dgeo.registration import *
 
 def test_icp(epochs):
     epoch1, epoch2 = epochs
+    epoch1.calculate_normals(radius=2.5)
+    epoch2.calculate_normals(radius=2.5)
 
-    trafo = iterative_closest_point(epoch1, epoch2)
+    trafo2 = point_to_plane_icp(epoch1, epoch2)
 
-    assert trafo.affine_transformation.shape == (4, 4)
+    assert trafo2.affine_transformation.shape == (4, 4)
