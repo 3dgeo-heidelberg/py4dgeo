8e6f598c7f9e5acdb6963f2b941d709af21b7cd7
Thomas Isensee <thomas.isensee@iwr.uni-heidelberg.de>
2025-04-09 15:22:48 +0200

    Minimil C++-only-executable for testing the normal computation via compute_multiscale_directions

---
+++ minimal_benchmark/compute_normals.cpp
@@ -0,0 +1,45 @@
+#include <Eigen/Dense>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include "testsetup.hpp"
+#include <py4dgeo/compute.hpp>
+#include <py4dgeo/epoch.hpp>
+#include <py4dgeo/kdtree.hpp>
+
+using namespace py4dgeo;
+
+int
+main(int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <path-to-pointcloud.xyz>\n";
+    return 1;
+  }
+
+  std::string filename = argv[1];
+  auto cloud = benchcloud_from_file(filename);
+
+  std::cout << "Point cloud with " << cloud->rows() << " points.\n";
+
+  Epoch epoch(*cloud);
+  epoch.kdtree.build_tree(10);
+
+  std::vector<double> normal_radii{ 1.0 };
+  std::vector<double> used_radii;
+  EigenNormalSet directions(cloud->rows(), 3);
+  EigenNormalSet orientation(1, 3);
+  orientation << 0, 0, 1;
+
+  auto start = std::chrono::high_resolution_clock::now();
+  compute_multiscale_directions(
+    epoch, *cloud, normal_radii, orientation, directions, used_radii);
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  std::cout << "compute_multiscale_directions executed in " << duration.count()
+            << " seconds.\n";
+  std::cout << directions.rows() << " normals computed.\n";
+
+  return 0;
+}
