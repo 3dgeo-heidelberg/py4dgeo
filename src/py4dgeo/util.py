2f1645cb0e8168610a60346ae52b2aaf7438e1e4
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2026-01-30 15:20:22 +0100

    Temporarily revert DOI to v2, because of erroneous upload

--- src/py4dgeo/util.py
+++ src/py4dgeo/util.py
@@ -16,7 +16,7 @@ from importlib import metadata
 import _py4dgeo
 
 # The current data archive URL
-TEST_DATA_ARCHIVE = "doi:10.5281/zenodo.16751962/"
+TEST_DATA_ARCHIVE = "doi:10.5281/zenodo.18378272/"
 PY4DGEO_REQUEST_HEADERS = {
     "User-Agent": "py4dgeo (https://github.com/3dgeo-heidelberg/py4dgeo)"
 }
